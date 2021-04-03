d0e3b74adad6eedd5c785470a5981078a4d9ea9c
Leah O'Gorman <leah.p.ogorman@gmail.com>
2021-04-03 18:20:41 -0400

    changes to router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import'react-bootstrap'
 import './App.css';
 import { Container, Navbar, Nav } from "react-bootstrap";
@@ -18,10 +18,10 @@ class App extends React.Component {
     this.state = {
       headerLinks: [
         {title: 'Home', path: '/portfolio'},
-        {title: 'About', path: '/portfolio/about'},
-        {title: 'Portfolio', path: '/portfolio/pastwork'},
-        {title: 'Contact', path: '/portfolio/contact'},
-        {title: 'Resume', path: '/portfolio/resume'}
+        {title: 'About', path: '/about'},
+        {title: 'Portfolio', path: '/pastwork'},
+        {title: 'Contact', path: '/contact'},
+        {title: 'Resume', path: '/resume'}
       ],
       home: {
         title: "Leah O'Gorman",
@@ -41,29 +41,27 @@ class App extends React.Component {
 
   render(){
     return [
-      <Router>
-        <Container className="p-0" fluid={true}>
-          <Navbar className="border-bottom justify-content-end" gb="transparent" expand="lg">
-            <Navbar.Brand><Link className="nav-link brand" to="/portfolio">Leah O'Gorman</Link></Navbar.Brand>
-            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
-            <Navbar.Collapse id="navbar-toggle">
-              <Nav className="ml-auto">
-                
-                <Link className="nav-link" to="/portfolio/about">About</Link>
-                <Link className="nav-link" to="/portfolio/pastwork">Portfolio</Link>
-                <Link className="nav-link" to="/portfolio/contact">Contact</Link>
-                {/* <Link className="nav-link" to="/portfolio/resume">Resume</Link> */}
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Route path="/portfolio" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>}/>
-          <Route path="/portfolio/about" exact render={() => <AboutPage title={this.state.about.title} text={this.state.about.text}/>}/>
-          <Route path="/portfolio/contact" exact render={() => <ContactPage title={this.state.contact.title} />}/>
-          <Route path="/portfolio/resume" exact render={() => <ResumePage />}/>
-          <Route path="/portfolio/pastwork" exact render={() => <PortfolioPage title={this.state.portfolio.title} subtitle={this.state.portfolio.subtitle} text={this.state.portfolio.text}/>}/>
-          <Footer />
-        </Container>
-      </Router>
+      <Container className="p-0" fluid={true}>
+        <Navbar className="border-bottom justify-content-end" gb="transparent" expand="lg">
+          <Navbar.Brand><Link className="nav-link brand" to="/portfolio">Leah O'Gorman</Link></Navbar.Brand>
+          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+              
+              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/pastwork">Portfolio</Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
+              {/* <Link className="nav-link" to="/portfolio/resume">Resume</Link> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Route path="/portfolio" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>}/>
+        <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} text={this.state.about.text}/>}/>
+        <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} />}/>
+        <Route path="/resume" exact render={() => <ResumePage />}/>
+        <Route path="/pastwork" exact render={() => <PortfolioPage title={this.state.portfolio.title} subtitle={this.state.portfolio.subtitle} text={this.state.portfolio.text}/>}/>
+        <Footer />
+      </Container>
     ]
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HashRouter from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
